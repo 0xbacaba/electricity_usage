39088b8485a7d1590a0072cde407d2c41c6e055b
Joshi <joshua@lipps.de>
2026-06-29 02:17:30 +0200

    greatly improve data preparation speed and code readability

--- converter.py
+++ converter.py
@@ -2,13 +2,14 @@ import csv
 from datetime import datetime
 from dateutil import parser
 from settings import ENTITY_IDS
+from collections import defaultdict
 
 # output format:
-# energy: { <datetime>: { bought: <float>, sold: <float> } }
+# energy: { <timestamp>: { bought: <float>, sold: <float> } }
 
 class Builder:
     def __init__(self):
-        self.values = {}  # in output format
+        self.values = defaultdict(lambda: {"bought": 0.0, "sold": 0.0})
         self.last_values = {}  # {<entity_id>: <float>}
 
     def add_absolute(self, entity_id, value, time):
@@ -16,26 +17,22 @@ class Builder:
             return
         value = float(value)
 
-        if entity_id not in self.last_values:
-            self.last_values[entity_id] = value
-
-        change = value - self.last_values[entity_id]
+        prev = self.last_values.get(entity_id)
         self.last_values[entity_id] = value
 
-        for eid, key in ENTITY_IDS.items():
-            if time not in self.values:
-                self.values[time] = {}
-            # we need to ensure every entry has both keys (bought and sold)
-            if key not in self.values[time]:
-                self.values[time][key] = 0
-            if eid == entity_id:
-                self.values[time][key] += change
+        if prev is None:
+            return
+
+        change = value - prev
+
+        key = ENTITY_IDS[entity_id]
+        self.values[time][key] += change
 
     def result(self):
-        return self.values
+        return dict(self.values)
 
-def floor_hour(time: datetime):
-    return datetime(time.year, time.month, time.day, time.hour, 0, 0, 0, time.tzinfo)
+def floor_hour(time: str):
+    return int(time[:13].replace("-", "").replace(":", "").replace("T", ""))
 
 def from_csv(file):
     with open(file, "r") as file:
@@ -43,7 +40,7 @@ def from_csv(file):
         output = Builder()
 
         for entry in data:
-            time = floor_hour(parser.parse(entry["last_changed"]))
+            time = floor_hour(entry["last_changed"])
             output.add_absolute(entry["entity_id"], entry["state"], time)
 
         return output.result()
